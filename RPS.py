3e0224b0efb2bbd95b2b4b3b2921477391666bd7
JR Pierce <jrpierce24@gmail.com>
2019-10-19 18:07:48 -0400

    Fuctions added
    Computer logic added to display random number as a string of either rock. paper or sissors.
    
    Changed win condition to be a check fuction rather than with in the loop
    
    Added Space function for quality of life.
    
    Play again Feature was added at the end.

--- RPS.py
+++ RPS.py
@@ -5,16 +5,27 @@ lose_counter = 0
 new = 0
 comp_num = 0
 
+
 def game():
     global win_counter
     global lose_counter
     global comp_num
+    win_counter = 0
+    lose_counter = 0
+    space(4)
 
-    while win_counter < 2 or lose_counter < 2:
+    while True:
+
         display()
+
+        space(3)
+
         read()
+
         comp_num = random.randint(1, 3)
-        print(comp_num)
+        computer(comp_num)
+
+        space(1)
         if compare() == 2:
             print("You Win!")
             win_counter += 1
@@ -23,7 +34,13 @@ def game():
             lose_counter += 1
         elif compare() == 3:
             print("Draw")
+        pause()
+        if check_wins() == 1 or check_wins() == 0:
+            print("Win Count: " + str(win_counter) + " " "Lose Count: " + str(lose_counter))
+            break
 
+    space(2)
+    play_again()
 
 
 def display():
@@ -44,6 +61,7 @@ def read():
             print("Please Re-enter your attack!")
         elif new == 1 or new == 2 or new == 3:
             good_data = True
+
 
 def check(data):
     if data == "Rock" or data == "rock" or data == "r" or data == "R":
@@ -77,6 +95,45 @@ def compare():
 
     elif comp_num == 1 and new == 2:
         return 2
+
+
+def computer(selection):
+    print("VS")
+    if selection == 1:
+        print("Rock")
+    if selection == 2:
+        print("Paper")
+    if selection == 3:
+        print("Scissors")
+
+
+def check_wins():
+    if win_counter == 2:
+        return 1
+    elif lose_counter == 2:
+        return 0
+
+
+def space(x):
+    if x == 1:
+        print(" ")
+    if x == 2:
+        for x in range(20): print(" ");
+    if x == 3:
+        print("")
+        print("")
+    if x == 4:
+        for x in range(45): print(" ")
+
+
+def pause():
+    space(1)
+    input("Press Enter to Continue....")
+
+def play_again():
+    aws = input("Would you like to play again you fool?  : ")
+    if aws == "yes" or aws == "Yes" or aws == "Y" or aws == "y":
+        game()
 
 
 game()
